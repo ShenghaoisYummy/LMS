65a17ef60c2a908b141e57f0bdebd7c97b583dbf
Shenghao <hsupisces@hotmail.com>
2018-05-06 13:08:04 +1000

    add basic framework

---
+++ LMS/LMS/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Data.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LMS.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("api/test")]
+        public IHttpActionResult Test()
+        {
+            using (LMSEntities context = new LMSEntities())
+            {
+                var students = context.Students.ToList();
+                return Ok(students);
+            }
+        }
+    }
+}
